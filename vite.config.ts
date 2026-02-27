d02c6d29ab1b7cea33e56cf1baa9100396e074eb
Ubuntu <ubuntu@localhost.localdomain>
2026-02-27 22:25:54 +0800

    config: allowedHosts 改为 true 允许所有域名

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
-    allowedHosts: ['hjscm.banjo-qilin.ts.net']
+    allowedHosts: true
   },
   resolve: {
     alias: {
